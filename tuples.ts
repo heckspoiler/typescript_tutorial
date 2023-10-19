abe2a11932d76c9ed420caa243422e7760401c1d
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-10-19 22:17:18 +0200

    quick excourse to types

--- tuples.ts
+++ tuples.ts
@@ -16,3 +16,10 @@ const pepsi: [string, boolean, number] = ['brown', true, 40];
 // this is a tuple with type annotations. We can also use type aliases to make the code more readable. this array always has a very specific ordering of types in it. If we now try to change the first element to a number, we get an error.
 
 // Type aliases
+
+type Drink = [string, boolean, number]; // types are used to create a new type alias instead of always having to write out the type annotation every time. This is not an array, it creates the idea of a tuple inside of our application.
+// we would now create the tuple like this:
+
+const pepsi2: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
