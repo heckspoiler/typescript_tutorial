0a4c99aec1d50fb254c0adce2663e9fb491063b8
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-10-20 00:10:39 +0200

    classes with fields up next

--- classes.ts
+++ classes.ts
@@ -1,15 +1,29 @@
 // a class is a blueprint to create an object with some fields (values) and methods (functions) to represent a "thing"
 
 class Vehicle {
-  drive(): void {
-    console.log('chugga chugga');
-  }
-  honk(): void {
+  protected honk(): void {
     console.log('boop boop');
   }
 }
 
-const vehicle = new Vehicle();
+// inheritance system in typescript, changed the drive method in class (overwritten it)
+// what are modifiers? modifiers are keywords that we can place on different methods and properties inside of a class
 
-vehicle.honk();
-vehicle.drive();
+// modifier 1: public: this method can be called anywhere, anytime (this is by default if we define a method or property)
+// modifier 2: private: this method can only be called by other methods in this class
+// modifier 3: protected: this method can be called by other methods in this class, or by other methods in child classes
+
+class Car extends Vehicle {
+  private drive(): void {
+    console.log('vroom vroom');
+  }
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car();
+car.startDrivingProcess();
+
+// classes in typescript
